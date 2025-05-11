988e9dce9aba05ec910841c096922818ca4b3bf4
Yuxiang <yuxiang32@gmail.com>
2025-05-11 15:47:56 +0800

    build: initialize project with basic structure and dependencies
    Add initial project files including `__init__.py`, `__main__.py`, `requirements.txt`, and `pyproject.toml`. This sets up the basic structure, dependencies, and build configuration for the `insta485generator` project.

---
+++ insta485generator/__init__.py
@@ -0,0 +1,3 @@
+"""
+insta485generator
+"""
---
+++ insta485generator/__main__.py
@@ -0,0 +1,9 @@
+"""Build static HTML site from directory of HTML templates and plain files."""
+
+
+def main():
+    """Top level command line interface."""
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
